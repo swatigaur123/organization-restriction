7e56ef4761cbc5cea460a973aa598f181bb20ef1
swatigaur123 <gaurcs123@gmail.com>
2016-06-09 16:12:48 +0530

    Rules added for organizations

--- model/rule.model.js
+++ model/rule.model.js
@@ -1,17 +1,59 @@
-var mongoose=require('mongoose');
+var mongoose = require('mongoose');
 
 var ruleSchema = new mongoose.Schema({
   experience: [Number],
   band: [String],
-  serviceType: {
-    type: 'String',
-    required: true
-  },
+  serviceType: String,
   rule: {
-    path: 'String',
-    value: 'String'
+    path: String,
+    value: String
   },
-  priority:'Number',
+  priority:Number
 });
 
- module.exports = mongoose.model('rule',ruleSchema);
+
+ruleSchema.statics.getRules = function(serviceType,bandId,exp,cb){
+  if( bandId && exp) {
+    criteria={band:bandId,experience: exp};
+    console.log('Inside Get of rules');
+    console.log('rules for data given');
+    if(serviceType)
+    criteria.serviceType=serviceType;
+    this.find(criteria,cb);
+  } else {
+    //type of status code has to be decided. Not sure ...
+    //res.status(400).end();
+    var error = new Error();
+    error.status = 404;
+    error.message = "Rule cannot be found"
+    cb(error);
+  }
+
+}
+
+ruleSchema.statics.createRule = function(band,experience,serviceType,path,value,priority,cb){
+
+    this.create({
+      band: band,
+      experience: experience,
+      serviceType: serviceType,
+      rule: {
+        path: path,
+        value: value
+      },
+      priority: priority
+    },cb);
+
+  }
+
+ruleSchema.statics.deleteRule = function(id,cb){
+
+  this.remove({_id:id},cb);
+}
+
+ruleSchema.statics.modifyRule = function(id,object,cb){
+  console.log(id);
+  this.findByIdAndUpdate(id,object,{new: true},cb);
+}
+
+module.exports = mongoose.model('rule',ruleSchema);
--- routes/rules.route.js
+++ routes/rules.route.js
@@ -1,38 +1,58 @@
 var express = require('express');
 var router = express.Router();
-var Rule = require('../model/rule.model.js');
+var rule = require('../model/rule.model');
 
-router.get('/:serviceType/band/:bandId/exp/:exp',function(req,res){
-  if(req.params.serviceType && req.params.bandId && req.params.exp) {
-    console.log('Inside Get of rules');
-    Rule.find({band: req.params.bandId, serviceType: req.params.serviceType, experience: req.params.exp},function(err,appliedRules) {
-      if(err) return next(err);
-      res.json(appliedRules);
-    });
-  } else {
-    //type of status code has to be decided. Not sure ...
-    res.status(400).end();
+router.get('/:serviceType?/band/:bandId/exp/:exp',function(req,res){
+  console.log("inside getRules");
+  var serviceType = req.params.serviceType;
+  var bandId =  req.params.bandId;
+  var exp =  req.params.exp;
+  rule.getRules(serviceType,bandId,exp,function(err,data){
+    console.log(err);
+    console.log(data);
+    res.json(data);
+  });
+
+});
+
+router.post('/', function(req,res) {
+  var body = req.body;
+  if(body) {
+//     var newrule=rule.createRule(body.band,body.experience,body.serviceType,body.rule.path,body.rule.value,body.priority)
+// console.log("newrule");
+// console.log(newrule);
+rule.createRule(body.band,body.experience,body.serviceType,body.rule.path,body.rule.value,body.priority,function(err,data) {
+  console.log(data);
+  res.json(data);
+});
   }
+
+  else
+    res.send("no value for body");
 });
 
 
-router.post('/', function(req,res) {
-  if(req.body) {
-    console.log("inside post");
-    var rule= new Rule({
-      band:req.body.band,
-      experience:req.body.experience,
-      serviceType:req.body.serviceType,
-      rule: {
-        path: req.body.rule.path,
-        value: req.body.rule.value
-      },
-      priority: req.body.priority
-    });
-    rule.save(function(err,savedRule){
-      res.json(savedRule);
-    });
-  }
+router.post('/deleteRule',function(req,res){
+  console.log(req.body.id);
+  console.log("in delete");
+  var id=req.body.id;
+
+rule.deleteRule(id,function(err){
+  if(err) console.log(err);
+res.json({msg:"deleted"});
+});
+});
+
+router.post('/modifyRule',function(req,res){
+
+var object = req.body.ruleObject;
+  //var id=req.body.id;
+  console.log(object);
+  console.log("in modify");
+  rule.modifyRule(object._id,object,function(err,data){
+  console.log(data);
+  res.json(data);
+});
 });
 
 module.exports = router;
